52fe2b493b0960ae5a137287e087481ab54e0bef
LoStack <stackpoint@users.noreply.github.com>
2020-06-19 16:12:41 -1000

    Initial commit
    Legacy mod: https://steamcommunity.com/sharedfiles/filedetails/?id=404530381
    
    Last updated: May 13, 2016

---
+++ common/defines/cn_defines.lua
@@ -0,0 +1,8 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NCountry.COLONIAL_CLAIM_BONUS = 0
+NDefines.NCountry.COLONIAL_CLAIM_VIOLATION = 0
+NDefines.NCountry.COLONY_VIOLAION_PAPAL_INFLUENCE_COST  = 0
+NDefines.NCountry.MAX_CROWN_COLONIES = 0
